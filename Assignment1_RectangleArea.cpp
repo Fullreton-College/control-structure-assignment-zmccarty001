8123afe952152fca780a01126d774f89e0fc5a29
Turner, Zachariah <z00114761@gmail.com>
2026-02-21 15:26:32 -0800

    code changes complete

--- Assignment1_RectangleArea.cpp
+++ Assignment1_RectangleArea.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main()
 {
-	
+	int length_One, width_One, length_Two, width_Two;
+    cout<<"Enter length one, width one, length two, and width two of the rectangles: \n";
+    cin>>length_One>>width_One>>length_Two>>width_Two;
+    int area_One = length_One * width_One;
+    int area_Two = length_Two * width_Two;
+
+    if (area_One > area_Two) {
+        cout<<"The 1st rectangle has the greater area";
+    }
+    else if (area_One < area_Two) {
+        cout<<"The 2nd rectangle has the greater area";
+    }
+    else {
+        cout<<"The areas are the same";
+    }
+
     return 0;
 }
 
